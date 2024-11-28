9e32da92c9c9cb75130380d72389544ef66cbc48
Kyrowin <ten.ben2010@mail.ru>
2024-11-28 20:39:47 +0500

    Add files

---
+++ script.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+const numStars = 512; // Increased number of stars
+
+class Star {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.radius = Math.random() * 2;
+        this.dy = Math.random() * 0.7 + 0.3; // Slightly faster movement
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+    }
+
+    update() {
+        this.y += this.dy;
+        if (this.y > canvas.height) {
+            this.y = 0; // Reset star to the top
+            this.x = Math.random() * canvas.width; // Randomize X position
+        }
+        this.draw();
+    }
+}
+
+function init() {
+    stars = [];
+    for (let i = 0; i < numStars; i++) {
+        stars.push(new Star());
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    stars.forEach(star => star.update());
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
+});
+
+init();
+animate();
